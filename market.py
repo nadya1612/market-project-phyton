2389ce6e666609d16665bd3bf273ef3ca7c4fd52
Nadya Divia <nadya.diviago1612@gmail.com>
2026-05-08 00:43:10 +0700

    refactor(comment): adding change data buah

--- market.py
+++ market.py
@@ -3,12 +3,13 @@
 
 
 
-
 nama = ["apel","jeruk", "anggur"]
 stock = ["20","15", "10"]
 harga = ["10_000","15_000", "20_000"]      
 index = [0,1,2]
 total_harga = 0
+keranjang = []
+
 
 
 while True: 
@@ -35,11 +36,37 @@ while True:
         nama.append(nama_buah)
         stock.append(stock_buah)
         harga.append(harga_buah)
+    
         print("daftar buah")
+        
         print(f"|{'Index':<10} {'|Nama' :<10} {'|Stock':<10} {'|Harga':<10}")
         print("-"*40)
         for i in range(len(nama)):
             print(f"{i:<10} |{nama[i]:<10} |{stock[i]:<10} |{harga[i]:<10}")
+        
+            
+        while True:
+            confir_msg = input ("Lanjut tambah buah (yes:jika lanjut)?")
+            if confir_msg.lower() == "yes": 
+                nama_buah = input("Masukkan nama buah : ")
+                stock_buah = input("Masukkan stock buah : ")
+                harga_buah = input("Masukkan harga buah : ")
+                nama.append(nama_buah)
+                stock.append(stock_buah)
+                harga.append(harga_buah)
+    
+                print("daftar buah")
+        
+                print(f"|{'Index':<10} {'|Nama' :<10} {'|Stock':<10} {'|Harga':<10}")
+                print("-"*40)
+                for i in range(len(nama)):
+                    print(f"{i:<10} |{nama[i]:<10} |{stock[i]:<10} |{harga[i]:<10}")
+            else:
+               
+                break
+                
+                
+                        
     elif angka == "3":
        
         print(f"{'Index':<10} {'|Nama' :<10} {'|Stock':<10} {'|Harga':<10}")
@@ -55,30 +82,52 @@ while True:
         print("Daftar Buah")
         for i in range(len(nama)):
             print(f"{i:<10} |{nama[i]:<10} |{stock[i]:<10} |{harga[i]:<10}")
+   
     elif angka == "4":
         print(f"{'Indeks':<10} {'|Nama':<10} {'|Stock':<10} {'|Harga':<10}")
         print("-"*40)
         for i in range(len(nama)):
-            print(f"{i:<10} |{nama[i]:<10} |{stock[i]:<10} |{harga[i]:<10}")
-
+            print(f"{i:<10} |{nama[i]:<10} |{stock[i]:<10} |{harga[i]:<10}")        
+        keranjang = []
         beli_index =int(input("Masukkan index buah yang ingin di beli :"))
         jumlah_beli = (input("Masukkan jumlah buah yang ingin di beli :"))
+        keranjang.append(nama[beli_index])
+        keranjang.append(jumlah_beli)
+        keranjang.append(harga[beli_index])
         if jumlah_beli > stock[beli_index]:
-            print(f"Maaf stock tidak cukup, stock {nama[beli_index]} tinggal {stock[beli_index]}")
+
+                print(f"Maaf stock tidak cukup, stock {nama[beli_index]} tinggal {stock[beli_index]}")
         else:
-            print("Isi chart :")
-            print(f"{'|Nama' :<10} {'|Qty':<10} {'|Harga':<10}")
-            while True:
-                lanjut =input("Mau beli yang lain lagi? (ya/tidak)")
-                if  lanjut == "ya":
-                    beli_index =int(input("Masukkan index buah yang ingin di beli :") )  
-                    jumlah_beli = int(input("Masukkan jumlah buah yang ingin di beli :"))
-                    total_beli = jumlah_beli * int(harga[beli_index])
-                    total_harga += total_beli
-                    print("Isi chart :")
-                    print(f"{'|Nama' :<10} {'|Qty':<10} {'|Harga':<10}")
-                    print(f"(|{nama[beli_index]:<10} |{jumlah_beli[beli_index]:<10} |{harga [beli_index]}:<10)")
+            print(f"{'Nama':<10} {'|Qty':<10} {'|Harga':<10}")
+            print("-"*30)
+            print(f"{nama[beli_index]:<10} |{jumlah_beli:<10} |{harga[beli_index]:<10}")
+        
 
-    
+        while True:
+            lanjut =input("Mau beli yang lain lagi? (ya/tidak)")
+            if  lanjut == "ya": 
+                keranjang = []
+                beli_index =int(input("Masukkan index buah yang ingin di beli :"))
+                jumlah_beli = (input("Masukkan jumlah buah yang ingin di beli :"))
+                keranjang.append(nama[beli_index])
+                keranjang.append(jumlah_beli)
+                keranjang.append(harga[beli_index])
+                nama_beli = keranjang[0]
+                jumlah_beli = keranjang[1]
+            if jumlah_beli > stock[beli_index]:
+            
+                while jumlah_beli > stock[beli_index]:
+
+                    print(f"Maaf stock tidak cukup, stock {nama[beli_index]} tinggal {stock[beli_index]}")
+                    break
+                else:
+                    print(f"{'Nama':<10} {'|Qty':<10} {'|Harga':<10}")
+                    print("-"*30)
+                    print(f"{nama[beli_index]:<10} |{jumlah_beli:<10} |{harga[beli_index]:<10}")
+            else:
+                break
 
 
+
+           
+            
